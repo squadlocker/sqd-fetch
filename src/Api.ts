02ec17312d6870c12df170f54c1d5cff9d9aa9ce
cronning <christopher.a.ronning@gmail.com>
2019-01-17 14:46:49 -0500

    return non-ok error codes as normal response, to be handled by client

--- src/Api.ts
+++ src/Api.ts
@@ -6,16 +6,6 @@ export default class Api extends AbstractApi implements IApi {
     if (!!this.loadingProvider && handleLoading) {
       this.loadingProvider.onResolve();
     }
-    if (!response.ok) {
-      // allows us to handle error status codes AND custom error responses in a catch
-      // block we pass in an Error object so that we can have access to the stacktrace.
-
-      // In some catch block, access the status with err.response.status
-      // access any custom response body with err.response.json().then(json => {...})
-      const e = new FetchError;
-      e.response = response;
-      throw e;
-    }
 
     if (response.status === 204) {
       return { status: 204, success: true };
